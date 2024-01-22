5dbc5214357b4fb2cfb6f8be421492057e1a06dc
Eugene <115879896+eugenezabrodski@users.noreply.github.com>
2024-01-22 13:24:27 +0300

    22.01.24

---
+++ FinancialAccounting/FinancialAccounting/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  FinancialAccounting
+//
+//  Created by Eugene on 22/01/2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var recordImage: UIImageView!
+    @IBOutlet weak var recordCategory: UILabel!
+    @IBOutlet weak var recordPrice: UILabel!
+}
--- FinancialAccounting/FinancialAccounting/ViewController.swift
+++ FinancialAccounting/FinancialAccounting/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet var numberFromKeyboard: [UIButton]! {
+        didSet {
+            for button in numberFromKeyboard {
+                button.layer.cornerRadius = 11
+            }
+        }
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    var stillTyping = false
+    var categoryName = ""
+    var displayValue = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        if stillTyping {
+            if displayLabel.text!.count < 15 {
+                displayLabel.text = displayLabel.text! + number
+            }
+        } else {
+            displayLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        displayLabel.text = "0"
+        stillTyping = false
+    }
+    
+    
+    @IBAction func categoryPressed(_ sender: UIButton) {
+        categoryName = sender.currentTitle ?? ""
+        displayValue = displayLabel.text!
+        displayLabel.text = "0"
+        stillTyping = false
+        
+        print(categoryName)
+        print(displayValue)
+    }
+    
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.recordCategory.text = "hello"
+        cell.recordImage.image = UIImage(systemName: "car")
+        return cell
+    }
+    
+    
 }
 
